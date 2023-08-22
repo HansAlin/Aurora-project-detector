f26b58000070f44badd42d121df7eebafc404773
Hans Alin <hansalin@gmail.com>
2023-08-22 16:52:28 +0200

    Webserver improvments

--- lib/WifiConection/src/WifiConnection.cpp
+++ lib/WifiConection/src/WifiConnection.cpp
@@ -3,86 +3,125 @@
 WiFiConnection::WiFiConnection(const char* ssid, const char* password)
   : ssid(ssid), password(password), server(80), longitude(0), latitude(0), zenit(0), utc_off(0), cloud_value_scale(0), temperature(0), humidity(0), aurora_point(0) {}
 
+const char* cssContent = R"(
+
+    body {
+      font-family: Arial, sans-serif;
+    }
+    form {
+      margin-bottom: 20px;
+    }
+    label {
+      display: block;
+      margin-bottom: 5px;
+      font-size: 18px;
+    }
+    input[type="text"], input[type="password"] {
+      width: 30%;
+      padding: 5px;
+      margin-bottom: 10px;
+      font-size: 18px;
+    }
+    input[type="submit"] {
+      background-color: #4CAF50;
+      color: white;
+      font-size: 18px;
+      border: none;
+      padding: 10px 20px;
+      cursor: pointer;
+    }
+    .styled-form {
+      margin-bottom: 20px;
+    })";
+
 void WiFiConnection::update() {
   WiFi.mode(WIFI_AP_STA);
-  WiFi.softAP(ssid, password);
-  Serial.println();
-  Serial.print("Access Point Mode IP address: ");
-  Serial.println(WiFi.softAPIP());
-  // if (WiFi.status() !=WL_CONNECTED) {
-  //   int count = 0;
-  //   Serial.println("Trying to access wifi");
-  //   while (WiFi.begin() != WL_CONNECTED && count < 10 ) {
-  //     Serial.print(".");
-  //     count += 1;
-  //     delay(500);
-  //   }
-  // }
+  if (WiFi.begin() == WL_CONNECTED) {
+    Serial.print("Local IPaddress: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    WiFi.softAP(ssid, password);
+    Serial.println();
+    Serial.print("Access Point Mode IP address: ");
+    Serial.println(WiFi.softAPIP());
+  }
+  
+  
   server.on("/", [this]() {
-    String html = "<html><body>";
+    
+    String html = "<html><head><style>";
+    html += cssContent;
+
+    html += "</style>";
+    html += "<script>";
+    html += "function redirectToHome() {";
+    html += "  console.log('Redirecting to home...');";
+    html += "  window.location.href = '/';";
+    html += "}";
+    html += "</script>";
+    html += "</head><body>";
+    html += "<center>";
+    
     html += "<h2>Values</h2>";
-    // html += "<p>Longitude: " + String(longitude) + "</p>";
-    // html += "<p>Latitude: " + String(latitude) + "</p>";
-    // html += "<p>Zenit: " + String(zenit) +  "</p>";
-    // html += "<p>UTC off: " + String(utc_off) + "</p>";
-    // html += "<p>Cloud value scale: " + String(cloud_value_scale) +  "</p>";
-    // html += "<p>Channel ID: " + channel_ID +  "</p>";
-    // html += "<p>Write API: " + writeAPI +  "</p>";
-    // html += "<p>Read API: " + readAPI +  "</p>";
-
-    html += "<form method='get' action='/update'>";
-    html += "<label>Change Longitude:</label>";
-    html += "<input type='text' name='longitude' value='" + String(longitude) + "'>";
-    html += "<input type='submit' value='Submit'><br>";
-    html += "</form>";
-
-    html += "<form method='get' action='/update'>";
-    html += "<label>Change Latitude:</label>";
-    html += "<input type='text' name='latitude' value='" + String(latitude) + "'>";
-    html += "<input type='submit' value='Submit'><br>";
-    html += "</form>";
-
-    html += "<form method='get' action='/update'>";
-    html += "<label>Change Zenit:</label>";
-    html += "<input type='text' name='zenit' value='" + String(zenit) + "'>";
-    html += "<input type='submit' value='Submit'><br>";
-    html += "</form>";
-
-    html += "<form method='get' action='/update'>";
-    html += "<label>Change UTC off:</label>";
-    html += "<input type='text' name='utc_off' value='" + String(utc_off) + "'>";
-    html += "<input type='submit' value='Submit'><br>";
-    html += "</form>";
-
-    html += "<form method='get' action='/update'>";
-    html += "<label>Change Cloud Value Scale:</label>";
-    html += "<input type='text' name='cloud_value_scale' value='" + String(cloud_value_scale) + "'>";
-    html += "<input type='submit' value='Submit'><br>";
-    html += "</form>";
-
-    html += "<form method='get' action='/update'>";
-    html += "<label>Change Channel ID:</label>";
-    html += "<input type='text' name='channel_id' value='" + channel_ID + "'>";
-    html += "<input type='submit' value='Submit'><br>";
-    html += "</form>";
-
-    html += "<form method='get' action='/update'>";
-    html += "<label>Change write API:</label>";
-    html += "<input type='text' name='write_API' value='" + writeAPI + "'>";
-    html += "<input type='submit' value='Submit'><br>";
-    html += "</form>";
-
-    html += "<form method='get' action='/update'>";
-    html += "<label>Change read API:</label>";
-    html += "<input type='text' name='read_API' value='" + readAPI + "'>";
-    html += "<input type='submit' value='Submit'><br>";
-    html += "</form>";
-
     html += "<p>Temperature: " + String(temperature) +  "</p>";
     html += "<p>Humidity: " + String(humidity) +  "</p>";
     html += "<p>Aurora points: " + String(aurora_point) +  "</p>";
     html += "<p>Time: " + formatedTime + "</p>";
     html += "<p>Day or Night: " + dayOrNight + "</p>";
+
+
+    html += "<h2>Change Paramters</h2>";
+
+    html += "<form method='get' action='/update' class='styled-form' onsubmit='redirectToHome()'>";
+
+    html += "<label>Change Longitude:</label>";
+    html += "<input type='text' name='longitude' value='" + String(longitude) + "'>";
+    html += "<input type='submit' value='Submit'><br>";
+    html += "</form>";
+
+    html += "<form method='get' action='/update' class='styled-form' onsubmit='redirectToHome()'>";
+    html += "<label>Change Latitude:</label>";
+    html += "<input type='text' name='latitude' value='" + String(latitude) + "'>";
+    html += "<input type='submit' value='Submit'><br>";
+    html += "</form>";
+
+    html += "<form method='get' action='/update' class='styled-form' onsubmit='redirectToHome()'>";
+    html += "<label>Change Zenit:</label>";
+    html += "<input type='text' name='zenit' value='" + String(zenit) + "'>";
+    html += "<input type='submit' value='Submit'><br>";
+    html += "</form>";
+
+    html += "<form method='get' action='/update' class='styled-form' onsubmit='redirectToHome()'>";
+    html += "<label>Change UTC off:</label>";
+    html += "<input type='text' name='utc_off' value='" + String(utc_off) + "'>";
+    html += "<input type='submit' value='Submit'><br>";
+    html += "</form>";
+
+    html += "<form method='get' action='/update' class='styled-form' onsubmit='redirectToHome()'>";
+    html += "<label>Change Cloud Value Scale:</label>";
+    html += "<input type='text' name='cloud_value_scale' value='" + String(cloud_value_scale) + "'>";
+    html += "<input type='submit' value='Submit'><br>";
+    html += "</form>";
+
+    html += "<form method='get' action='/update' class='styled-form' onsubmit='redirectToHome()'>";
+    html += "<label>Change Channel ID:</label>";
+    html += "<input type='text' name='channel_id' value='" + channel_ID + "'>";
+    html += "<input type='submit' value='Submit'><br>";
+    html += "</form>";
+
+    html += "<form method='get' action='/update' class='styled-form' onsubmit='redirectToHome()'>";
+    html += "<label>Change write API:</label>";
+    html += "<input type='text' name='write_API' value='" + writeAPI + "'>";
+    html += "<input type='submit' value='Submit'><br>";
+    html += "</form>";
+
+    html += "<form method='get' action='/update' class='styled-form' onsubmit='redirectToHome()'>";
+    html += "<label>Change read API:</label>";
+    html += "<input type='text' name='read_API' value='" + readAPI + "'>";
+    html += "<input type='submit' value='Submit'><br>";
+    html += "</form>";
+
 
 
 
@@ -96,9 +135,9 @@ void WiFiConnection::update() {
     html += "<input type='submit' value='Connect'>";
     html += "</form>";
 
-    
+    html += "</center>";
     html += "</body></html>";
-
+    
 
     server.send(200, "text/html", html);
   });
@@ -131,7 +170,11 @@ void WiFiConnection::update() {
 
 
 
-    server.send(200, "text/plain", "Values updated");
+    // server.send(200, "text/plain", "Values updated");
+    // delay(1000);
+    server.sendHeader("Location", "/");
+    server.send(303);
+   
   });
 
   server.on("/connect", [this]() {
@@ -144,8 +187,10 @@ void WiFiConnection::update() {
     WiFi.begin(ssid, password);
     Serial.print("Local IPaddress: ");
     Serial.println(WiFi.localIP());
-    server.send(200, "text/plain", "Wi-Fi connected!");
-    delay(1000);
+    // server.send(200, "text/plain", "Wi-Fi connected!");
+    // delay(1000);
+    server.sendHeader("Location", "/");
+    server.send(303);
     //ESP.reset();
   });
 
@@ -155,9 +200,6 @@ void WiFiConnection::update() {
   Serial.println("Web server started");
 }
 
-// void WiFiConnection::connect() {
-
-// }
 
 void WiFiConnection::handleClient() {
   server.handleClient();
